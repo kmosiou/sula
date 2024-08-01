35482cca5dec478a748bbd4e900c570a1669bfa4
Korina Mosiou <158480979+kmosiou@users.noreply.github.com>
2024-08-01 16:17:44 +0300

    Update test_wg_A.py

--- test_wg_A.py
+++ test_wg_A.py
@@ -12,6 +12,45 @@ filepath = 'axeff_c.pickle' #load effective fetch
 with open(filepath, 'rb') as file:
     axeff = pickle.load(file)
 
+# previously
+#filepath = 'a16_windsea.pickle'
+#with open(filepath, 'rb') as file:
+#    a16 = pickle.load(file)
+#    
+#filepath = 'a17_wsean.pickle'
+#with open(filepath, 'rb') as file:
+#    a17 = pickle.load(file)
+#    
+#filepath = 'a18_wsea.pickle'
+#with open(filepath, 'rb') as file:
+#    a18 = pickle.load(file)
+#    
+#filepath = 'a19_wsean.pickle'
+#with open(filepath, 'rb') as file:
+#    a19 = pickle.load(file)
+#    
+#filepath = 'a20_wsean.pickle'
+#with open(filepath, 'rb') as file:
+#    a20 = pickle.load(file)
+#    
+#filepath = 'a21_wsean.pickle'
+#with open(filepath, 'rb') as file:
+#    a21 = pickle.load(file)
+#    
+#filepath = 'a22_windsea.pickle'
+#with open(filepath, 'rb') as file:
+#    a22 = pickle.load(file)
+#    
+#filepath = 'a23_windsea.pickle'
+#with open(filepath, 'rb') as file:
+#    a23 = pickle.load(file)
+#    
+#filepath = 'a24_windsea.pickle'
+#with open(filepath, 'rb') as file:
+#    a24 = pickle.load(file)
+#
+#aws = xr.merge([a16, a17, a18, a19, a20, a21, a22, a23, a24])
+
 filepath = 'awindsea.pickle' #load windsea_spec2d
 with open(filepath, 'rb') as file:
     aws = pickle.load(file)
@@ -25,36 +64,45 @@ u10 = u10a.where(u10a>8, drop=True) # Filter windspeed
 wdd = wd.where((wd >= 90) & (wd <=210), drop=True) # Filter wind direction
 indud = np.intersect1d(u10['time'].values, wdd['time'].values) 
 ud412 = wdd['WindDirection'].sel(time=indud)
-udr = np.round(ud412/10)*10
-xx = np.empty(len(udr))
-for i in range(len(udr)):
-    if udr[i]==90:
-        xx[i]=axeff[0]
-    elif udr[i]==100:
-        xx[i]=axeff[1]
-    elif udr[i]==110:
-        xx[i]=axeff[2]
-    elif udr[i]==120:
-        xx[i]=axeff[3]
-    elif udr[i]==130:
-        xx[i]=axeff[4]  
-    elif udr[i]==140:
-        xx[i]=axeff[5]    
-    elif udr[i]==150:
-        xx[i]=axeff[6]
-    elif udr[i]==160:
-        xx[i]=axeff[7]  
-    elif udr[i]==170:
-        xx[i]=axeff[8]    
-    elif udr[i]==180:
-        xx[i]=axeff[9] 
-    elif udr[i]==190:
-        xx[i]=axeff[10]  
-    elif udr[i]==200:
-        xx[i]=axeff[11]  
-    else:
-        xx[i]=axeff[12]
-        
+udr = np.round(ud412 / 10).values * 10
+
+#xx = np.empty(len(udr))
+#for i in range(len(udr)):
+#    if udr[i]==90:
+#        xx[i]=axeff[0]
+#    elif udr[i]==100:
+#        xx[i]=axeff[1]
+#    elif udr[i]==110:
+#        xx[i]=axeff[2]
+#    elif udr[i]==120:
+#        xx[i]=axeff[3]
+#    elif udr[i]==130:
+#        xx[i]=axeff[4]  
+#    elif udr[i]==140:
+#        xx[i]=axeff[5]    
+#    elif udr[i]==150:
+#        xx[i]=axeff[6]
+#    elif udr[i]==160:
+#        xx[i]=axeff[7]  
+#    elif udr[i]==170:
+#        xx[i]=axeff[8]    
+#    elif udr[i]==180:
+#        xx[i]=axeff[9] 
+#    elif udr[i]==190:
+#        xx[i]=axeff[10]  
+#    elif udr[i]==200:
+#       xx[i]=axeff[11]  
+#    else:
+#        xx[i]=axeff[12]
+
+# Create a dictionary for axeff mapping
+axeff_map = {90: axeff[0], 100: axeff[1], 110: axeff[2], 120: axeff[3], 130: axeff[4], 
+             140: axeff[5], 150: axeff[6], 160: axeff[7], 170: axeff[8], 180: axeff[9], 
+             190: axeff[10], 200: axeff[11], 210: axeff[12]}
+
+# Map the rounded wind directions to axeff values
+xx = np.array([axeff_map.get(int(dir), axeff[12]) for dir in udr])
+
 u10b = u10.sel(time=indud)
 afetch = (grav * xx)/u10b['WindSpeed']**2 # Calculate dimensionless fetch
 windsea = aws['h_SPEC_windsea'].sel(time=indud)
