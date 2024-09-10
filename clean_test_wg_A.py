15ff0fc0e745b39df0287fc02a1213b595030e79
Korina Mosiou <158480979+kmosiou@users.noreply.github.com>
2024-09-10 15:08:39 +0300

    Update clean_test_wg_A.py

--- clean_test_wg_A.py
+++ clean_test_wg_A.py
@@ -68,7 +68,7 @@ adjusted_data_array = np.abs(u10c - aligned_means)
 # Resample and adjust wind direction data
 wdc = wdd.sel(time=ec['time'].values)
 resampled = wdc.resample(time='3h')
-three_hour_circular_means = resampled.reduce(circmean, high=360, low=0)
+three_hour_circular_means = resampled.reduce(circmean, high=360, low=0) # correction needed, circmean needs angles in radians
 aligned_circular_means = three_hour_circular_means.reindex(time=wdc.time, method='ffill')
 adjusted_circdata_array = np.abs(wdc['WindDirection'] - aligned_circular_means)
 thetadd = adjusted_circdata_array.where(~np.isnan(adjusted_circdata_array), drop=True)
