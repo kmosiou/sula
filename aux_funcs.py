4a06d3df784b660a1252312367ce5eac01ab80d0
Korina Mosiou <158480979+kmosiou@users.noreply.github.com>
2024-10-31 12:20:31 +0200

    Update aux_funcs.py

--- aux_funcs.py
+++ aux_funcs.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import xarray as xr
 import numpy as np
+from scipy.stats import circmean
 
 def angdif(ang1, ang2):
 # ang* in degrees
@@ -10,3 +11,25 @@ def angdif(ang1, ang2):
     dtheta = xr.where(dtheta > 180, 360 - dtheta, dtheta)
 
     return dtheta
+
+def process_wind_direction(winddir, interval='1h', max_deviation=15):
+    # Convert wind direction to radians
+    wdc = np.deg2rad(winddir)
+    
+    # Resample the wind direction data to the specified interval and calculate the mean
+    hourlywind = wdc.resample(time=interval)
+    hourlywindmean = hourlywind.reduce(circmean, high=360, low=0)
+    
+    # Reindex to match the original timestamps, using forward fill for missing values
+    hourlywindmeann = hourlywindmean.reindex(time=wdc.time, method='ffill')
+    
+    # Calculate wind deviation
+    winddev = np.abs(winddir - np.rad2deg(hourlywindmeann))
+    winddev = winddev % 360
+    winddev = winddev.where(~np.isnan(winddev), drop=True)
+    winddev = xr.where(winddev > 180, 360 - winddev, winddev)
+    
+    # Filter deviations within the specified maximum deviation limit
+    limwinddir = winddev.where(winddev <= max_deviation, drop=True)
+    
+    return limwinddir
